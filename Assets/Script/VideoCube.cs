46cad4c1f4af1e0c77137699bc66930770c92a28
Sergio Garzon <sggarzonutn@gmail.com>
2020-05-16 13:22:28 -0300

    Se sube el proyecto para insertar video

---
+++ Assets/Script/VideoCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoCube : MonoBehaviour
+{
+    public GameObject objetoVideo;
+    private Renderer render;
+    private Material mat;
+
+
+    void Start()
+    {
+        this.render = this.objetoVideo.GetComponent<Renderer>();
+        this.mat = this.render.material;
+        
+    }
+
+}
